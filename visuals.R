e232fc2e170e85dc88793172f2c23ee77acf3467
roburger <roburger1@gmail.com>
2023-09-05 19:14:33 +0200

    New Paper push

--- visuals.R
+++ visuals.R
@@ -91,9 +91,78 @@ ggplotBar = function(ModelsToPlot, Truth, statistic="RMSE", ylab=NULL, digits=0,
   
 }
 
+ggplotBarNew = function(ModelsToPlot, Truth, statistic="RMSE", ylab=NULL, digits=0, textsize=2.5, textvjust=-0.1, ylim=40, ...)
+{
+  if (is.null(ylab)) ylab=statistic
+  ModelStats=NULL
+  for (i in 1:length(ModelsToPlot))
+  {
+    LMM = AccuracyStatTable(ModelsToPlot[[i]], Truth[[i]], ...)[statistic]
+    LMM[[paste0(statistic,"R")]] = round(LMM[[statistic]], digits = digits) # For printing rounded numbers
+    LMM$Model=names(ModelsToPlot)[i]
+    ClassNames = prettyNames(rownames(LMM))
+    
+    #ClassNames = rownames(LMM)
+    #LMM$Class=factor(ClassNames, c("Overall", ClassNames[ClassNames != "Overall"]))
+    LMM$Class=factor(ClassNames, c("Overall",ClassNames[ClassNames != "Overall"]))
+    #LMM$Class=factor(prettyNames)
+    
+    ModelStats = rbind(ModelStats, LMM)
+  }
+  ModelStats$Model = factor(ModelStats$Model, levels = names(ModelsToPlot))
+  print(ModelStats)
+  #return(ModelStats)
+  # ggplot(ModelStats, aes_string("Class", statistic, fill="Year")) +
+  ggplot(ModelStats, aes_string("Class", statistic, fill="Model")) +
+    geom_col(position = "dodge", colour="black") +
+    geom_text(aes_string(label=sprintf("%s", paste0(statistic,"R"))), position=position_dodge(width = 0.9), vjust=textvjust, size=textsize) +
+    #scale_fill_manual(name = "Class", values = GetCommonClassColours(TRUE, 0.1)) +
+    coord_cartesian(clip = 'off') + ylab(ylab) + theme(axis.title.x=element_blank()) +
+    scale_y_continuous(limits = c(0, ylim), breaks = seq(0,50,10))
+  
+}
+
+
+## Make a new Barplot
+# Switch the years with the models
+
+plot2015 = ggplotBarNew(list("Basic RF"=basic2015,"Recurrent RF"=rec2015,"Markov"=markov2015),
+                      list(change2015[,loadClassNames()],
+                           change2015[,loadClassNames()],
+                           change2015[,loadClassNames()]),
+                      digits = 1, textsize = 2.25, textvjust=-0.5) + 
+  theme(legend.position="none") + ylab("2015") + theme(axis.title.y = element_text(face="bold",angle=0,vjust=0.5,margin=margin(t=0,r=10,b=0,l=0)))
+
+plot2016 = ggplotBarNew(list("Basic RF"=basic2016,"Recurrent RF"=rec2016,"Markov"=markov2016),
+                        list(change2016[,loadClassNames()],
+                             change2016[,loadClassNames()],
+                             change2016[,loadClassNames()]),
+                        digits = 1, textsize = 2.25, textvjust=-0.5) + 
+  theme(legend.position="none") + ylab("2016") + theme(axis.title.y = element_text(face="bold",angle=0,vjust=0.5,margin=margin(t=0,r=10,b=0,l=0)))
+
+plot2017 = ggplotBarNew(list("Basic RF"=basic2017,"Recurrent RF"=rec2017,"Markov"=markov2017),
+                        list(change2017[,loadClassNames()],
+                             change2017[,loadClassNames()],
+                             change2017[,loadClassNames()]),
+                        digits = 1, textsize = 2.25, textvjust=-0.5) + 
+  theme(legend.position="none") + ylab("2017") + theme(axis.title.y = element_text(face="bold",angle=0,vjust=0.5,margin=margin(t=0,r=10,b=0,l=0)))
+
+plot2018 = ggplotBarNew(list("Basic RF"=basic2018,"Recurrent RF"=rec2018,"Markov"=markov2018),
+                        list(change2018[,loadClassNames()],
+                             change2018[,loadClassNames()],
+                             change2018[,loadClassNames()]),
+                        digits = 1, textsize = 2.25, textvjust=-0.5) + 
+  theme(legend.position="bottom") + ylab("2018") + theme(axis.title.y = element_text(face="bold",angle=0,vjust=0.5,margin=margin(t=0,r=10,b=0,l=0)))
+
+
+pdf("~/Thesis/Paper/visualizations/barplots-perclass/rmse-models-bold.pdf", width=1272/175, height=(634*2.2)/175) #height 2.2 ipv 2
+gridExtra::grid.arrange(plot2015, plot2016, plot2017, plot2018, heights=c(30, 30, 30, 40))#nrow=3
+dev.off()
 
 
 
+
+## old below
 # textsize was 1.9, now 2.4
 
 #ggplotBar(list("2015"=basic2015,"2016"=basic2016,"2017"=basic2017,"2018"=basic2018),
@@ -121,7 +190,7 @@ MarkovPlot = ggplotBar(list("2015"=markov2015,"2016"=markov2016,"2017"=markov201
                        digits = 1, textsize = 2.25, textvjust=-0.5)+ 
   theme(legend.position="bottom")+ ylab("c)") + theme(axis.title.y = element_text(angle=0,vjust=0.5,margin=margin(t=0,r=10,b=0,l=0)))
 
-pdf("../../Overig/Visualizations/barplots-perclass/rmse-all-new-test.pdf", width=1272/175, height=(634*2.2)/175) #height 2.2 ipv 2
+pdf("../../visualizations/barplots-perclass/rmse-all-new-test.pdf", width=1272/175, height=(634*2.2)/175) #height 2.2 ipv 2
 gridExtra::grid.arrange(BasicPlot, RecPlot, MarkovPlot, heights=c(40, 40, 50))#nrow=3
 dev.off()
 
